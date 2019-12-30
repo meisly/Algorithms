d67055f44526adca5c1c15383b7b2daa0eec54ee
Tierra <sr.undershaft@gmail.com>
2019-12-29 23:39:36 -0600

    implemented mergeSort and countInversions algorithms

--- Divide_Conquer/mergeSort.js
+++ Divide_Conquer/mergeSort.js
@@ -9,20 +9,20 @@ const getData = () => {
         return numberArr;
     }
 }
-let inversions = 0;
+
 const mergeSort = (IntArr, length) => {
     if (IntArr.length <= 1) {
         return IntArr;
     }
-
     let midpt = Math.floor(length / 2);
     let left = IntArr.slice(0, midpt);
     let right = IntArr.slice(midpt, length);
 
     let sortedL = mergeSort(left, left.length);
     let sortedR = mergeSort(right, right.length);
-    return (merge(sortedL, sortedR));
-   
+
+    return (merge(sortedL, sortedR))
+
 }
 const merge = (sortedL, sortedR) => {
     let merged = [];
@@ -37,26 +37,22 @@ const merge = (sortedL, sortedR) => {
                 i++;
             } else if (sortedR[j] < sortedL[i]) {
                 merged[k] = sortedR[j];
-                j++;
-                inversions += sortedL.slice(i).length;
+                j++
             }
         }else {
             if(sortedL[i]){
-                merged[k] = sortedL[i];
-                i++
+                merged[k] = sortedL[i]
             } else {
-                merged[k] = sortedR[j];
-                j++;
+                merged[k] = sortedR[j]
             }
         } 
     }
-    console.log(inversions);
+
     return merged
 
 }
 numberArr = getData();
-let sample = numberArr.slice(0, 5);
-
-
-console.log(mergeSort(sample, sample.length));
+let sample = numberArr.slice(0, 10);
+console.log(sample)
+console.log(mergeSort(numberArr, numberArr.length));
 
