20fed2575f40f6d91a61c66be23e19d508bbfcb9
Tierra <sr.undershaft@gmail.com>
2020-02-08 17:55:11 -0600

    created betterMinCut.  dropped from 20 ms to ~8 ms per karger call

---
+++ Unit3/betterMinCut.js
@@ -0,0 +1,173 @@
+const fs = require('fs');
+
+function getData() {
+    let data = fs.readFileSync('kargerMinCut.txt', 'utf8');
+
+    if (!data) return [];
+    else {
+        let roughAr = data.split("\r\n")
+            .map(elem => elem.split("\t")
+                .filter(ele => ele != "")
+                .map(el => el.trim()))
+            .filter(el => el.length > 1);
+
+        return roughAr;
+    }
+}
+// let nodes = [[id/list of ids], n connected nodes, n2, n3,...], [# of node/nodes, n connected nodes], ....[]];
+// when an edge is contracted, the id of later node is pushed into the id position of the node w/ lower index and connected nodes are added
+// the later node is deleted and replaced with [null, position of node containing this node]
+// let edges = [[ref, ref], [ref, ref]]
+
+function processData(roughAr) {
+    let finalAr =[];
+        
+    for(let i = 0; i < roughAr.length; i++){
+        let nodeHash = {
+            ids: [roughAr[i][0]],
+            connects: roughAr[i].slice(1),
+            points: null
+        };
+        finalAr[i] = nodeHash;
+    }
+    return finalAr;
+}
+function getEdges(array) {
+    let edges = [];
+    let edgesTracker = {};
+    for (let i = 0; i < array.length; i++) {
+
+        for (let j = 1; j < array[i].length; j++) {
+            if (!edgesTracker[array[i][0] + array[i][j]] && !edgesTracker[array[i][j] + array[i][0]]) {
+                edgesTracker[array[i][0] + array[i][j]] = true;
+                edges.push([parseInt(array[i][0]) - 1, parseInt(array[i][j]) - 1]);
+            }
+
+        }
+    }
+    return edges;
+}
+function shuffle(arr) {
+    let i, j;
+    for (i = arr.length - 1; i > 0; i--) {
+        j = Math.floor(Math.random() * (i + 1));
+        [arr[i], arr[j]] = [arr[j], arr[i]];
+    }
+    return arr;
+};
+
+function contractEdge(array, edges, edge) {
+
+    let goodEdge = false;
+    let n1, n2;
+    while (goodEdge === false) {
+        if (edges[edge]) {    //make sure edge exists and hasn't been deleted
+            [n1, n2] = edges[edge];
+
+            //if node has been contracted will trace it to its current index
+            n1 = getFinalNode(n1, array);
+            n2 = getFinalNode(n2, array);
+
+            if (n1 == n2) {
+                edge++;
+            } else {
+                goodEdge = true;
+            }
+        } else {
+            edge++
+        }
+    }
+    //pushes later node into earlier node and rewrites later one to refer to combined node
+    let min, max;
+    if (n1 < n2) {
+        min = n1;
+        max = n2;
+    } else {
+        min = n2;
+        max = n1;
+    }
+    array[min].ids.push(...array[max].ids);
+    array[min].connects.push(...array[max].connects);
+    array[max].ids = null;
+    array[max].connects = null;
+    array[max].points = min;
+    edge++;
+
+    return edge;
+};
+
+function removeSelfLoops(node) {
+    let {
+        ids,
+        connects
+    } = node;
+
+    let selfLoopCount = 0;
+    for (let item of ids) {
+        for (let c = 0; c < connects.length; c++) {
+            if (connects[c] == item) {
+                [connects[selfLoopCount], connects[c]] = [connects[c], connects[selfLoopCount]];
+                selfLoopCount++;
+            }
+        }
+    }
+
+    connects.splice(1, selfLoopCount)
+}
+
+function getFinalNode(index, nodes) {
+    //if node being checked has been contracted into another node this will get the index of that node
+    if (nodes[index].ids === null) {
+        return getFinalNode(nodes[index].points, nodes);
+    } else {
+        return index;
+    }
+};
+function kargerMin(nodes, edges) {
+    let min;
+    let counter = nodes.length;
+    edges = shuffle(edges);
+    let edgeCounter = 0;
+    for (counter; counter > 2; counter--) {
+        edgeCounter = contractEdge(nodes, edges, edgeCounter);
+    }
+    let ab = [];
+    for (let i = 0; i < nodes.length; i++) {
+
+        if (nodes[i].ids != null) {
+            removeSelfLoops(nodes[i]);
+            ab.push(nodes[i]);
+        }
+    }
+    if (ab[0].connects.length === ab[1].connects.length) {
+        min = ab[0].connects.length;
+    } else {
+        min = Infinity;
+    }
+    return min;
+}
+let min = Infinity;
+
+let sample = getData();
+let edges = getEdges(sample);
+
+for (let i = 0; i < 200 ** 2 * Math.log(200); i++) {
+    if (i % 1000 == 0) {
+        console.time("mili");
+    }
+
+    let nodes = processData(sample);
+    let val = kargerMin(nodes, edges);
+    if (i % 1000 == 0) {
+        console.timeLog("mili");
+    }
+    if (val < min) {
+        min = val;
+    }
+
+    if (i % 1000 === 0) {
+        console.log(`i = ${i} \nmin = ${min} `);
+
+    }
+}
+console.log(min);
--- Unit3/naiveMinCut.js
+++ Unit3/naiveMinCut.js
@@ -5,7 +5,7 @@ function getData() {
 
     if (!data) return [];
     else {
-        let roughAr = data.split("\r\n").map(elem => elem.split("\t").filter(ele => ele != "" )).filter(el => el.length > 1 );
+        let roughAr = data.split("\r\n").map(elem => elem.split("\t").filter(ele => ele != "").map(el => el.trim())).filter(el => el.length > 1);
         return roughAr;
     }
 }
@@ -17,87 +17,127 @@ function getData() {
 function processData(array) {
     let edges = [];
     let edgesTracker = {};
-    for(let i = 0; i < array.length; i++){
+    for (let i = 0; i < array.length; i++) {
 
-        for (let j = 1; j < array[i].length; j++){
-            if(!edgesTracker[array[i][0]+array[i][j]] && !edgesTracker[array[i][j]+array[i][0]]){
-                edgesTracker[array[i][0]+array[i][j]] = true;
-                edges.push([parseInt(array[i][0])-1, parseInt(array[i][j])-1])
+        for (let j = 1; j < array[i].length; j++) {
+            if (!edgesTracker[array[i][0] + array[i][j]] && !edgesTracker[array[i][j] + array[i][0]]) {
+                edgesTracker[array[i][0] + array[i][j]] = true;
+                edges.push([parseInt(array[i][0]) - 1, parseInt(array[i][j]) - 1])
             }
-            
+
         }
         array[i][0] = [array[i][0]];
     }
-    return {nodes: array, edges: edges};
+    return { nodes: array, edges: edges };
 }
-function contractEdge(array) {
+function contractEdge(array, edges) {
     let randomEdge = Math.floor(Math.random() * edges.length);
-    if ( edges[randomEdge] ){    //make sure edge exists and hasn't been deleted
-        let [n1, n2] = edges[randomEdge];
-        edges[randomEdge] = false;
-
-        //if node has been contracted will trace it to its current index
-        n1 = getFinalNode(n1);
-        n2 = getFinalNode(n2);
-        //pushes later node into earlier node and rewrites later one to refer to combined node
-        if (n1 < n2) {
-            array[n1][0] = [...array[n1][0], ...array[n2][0]];
-            array[n1] = [...array[n1], ...array[n2].slice(1)];
-            array[n2] = [null, n1];
-        } else if (n2 < n1 ) {
-            array[n2][0] = [...array[n2][0], ...array[n1][0]];
-            array[n2] = [...array[n2], ...array[n1].slice(1)];
-            array[n1] = [null, n2];
-        } else if( n2 == n1) {
+    let goodEdge = false;
+    let n1, n2;
+    while (goodEdge === false) {
+        if (edges[randomEdge]) {    //make sure edge exists and hasn't been deleted
+            [n1, n2] = edges[randomEdge];
             edges[randomEdge] = false;
-            contractEdge(array);
+
+            //if node has been contracted will trace it to its current index
+            n1 = getFinalNode(n1, array);
+            n2 = getFinalNode(n2, array);
+
+            if (n1 == n2) {
+                randomEdge = (randomEdge + 1) % edges.length;
+            } else {
+                goodEdge = true;
+            }
+        } else {
+            randomEdge = (randomEdge + 1) % edges.length;
         }
-    } else {
-        edges[randomEdge] = false;
-        contractEdge(array);
-        
     }
+    //pushes later node into earlier node and rewrites later one to refer to combined node
+    if (n1 < n2) {
+        array[n1][0] = [...array[n1][0], ...array[n2][0]];
+        array[n1] = [...array[n1], ...array[n2].slice(1)];
+        array[n2] = [null, n1];
+    } else if (n2 < n1) {
+        array[n2][0] = [...array[n2][0], ...array[n1][0]];
+        array[n2] = [...array[n2], ...array[n1].slice(1)];
+        array[n1] = [null, n2];
+    }
+    return array;
 };
 
 function removeSelfLoops(node) {
     let self = node[0];
     let selfLoopCount = 0;
-    for( item in self ){
-        for(let c = 1; c < node.length; c++){
-            if ( node[c] == item ){
-                [node[1], node[c]] = [node[c], node[1]];
+    for (let item of self) {
+        for (let c = 1; c < node.length; c++) {
+            if (node[c] == item) {
+                [node[selfLoopCount + 1], node[c]] = [node[c], node[selfLoopCount + 1]];
                 selfLoopCount++;
             }
         }
     }
+    
     node.splice(1, selfLoopCount)
 }
 
-function getFinalNode(index) {
+function getFinalNode(index, nodes) {
     //if node being checked has been contracted into another node this will get the index of that node
-    if(nodes[index][0] === null ){                      
-        return getFinalNode(nodes[index][1]);
-    }else {
+    if (nodes[index][0] === null) {
+        return getFinalNode(nodes[index][1], nodes);
+    } else {
         return index;
     }
 };
-function kargerMin() {
+function kargerMin(nodes, edges) {
+    let min;
     let counter = nodes.length;
-    for( counter; counter > 2; counter--){
-        contractEdge(nodes);
-        if(counter === 4) {
-            console.log(nodes)
-        }    
+    for (counter; counter > 2; counter--) {
+        nodes = contractEdge(nodes, edges);
+        if (counter === 4) {
+
+        }
     }
+    let ab = [];
+    for (let i = 0; i < nodes.length; i++) {
 
+        if (nodes[i].length > 2) {
+            // console.log(`before: ${nodes[i].length-1}`);
+            removeSelfLoops(nodes[i]);
+
+            // console.log(`after: ${nodes[i].length-1}`)
+
+            // console.log(nodes[i].slice(1));
+            ab.push(nodes[i]);
+        }
+    }
+    if (ab[0].length === ab[1].length) {
+        min = ab[0].length;
+    } else {
+        min = Infinity;
+    }
+    return min;
 }
-
-let sample = getData()
-let processed = processData(sample)
-let nodes = processed.nodes;
-let edges = processed.edges;
+let min = Infinity;
 
 
-let noco = 0;
 
-kargerMin();
+
+for (let i = 0; i < 200**2 * Math.log(200); i++) {
+    if( i % 1000 == 0){
+        console.time("mili");
+    }
+    let sample = getData()
+    let processed = processData(sample)
+    let {nodes, edges } = processed;
+    let val = kargerMin(nodes, edges);
+    if( i % 1000 == 0){
+        console.timeLog("mili");
+    }
+    if (val < min) {
+        min = val;
+    }
+    if(i % 100 === 0){
+        console.log(`i = ${i} \nmin = ${min} `)
+    }
+}
+console.log(min);
